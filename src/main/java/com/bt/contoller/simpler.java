ea267e13ce410083c53842087d81564451bff6ae
Pooja0504 <jadhavpoojasopan@gmail.com>
2021-02-11 15:48:37 +0530

    simpler new

--- src/main/java/com/bt/contoller/simpler.java
+++ src/main/java/com/bt/contoller/simpler.java
@@ -60,4 +60,115 @@ public class QualityGateCommitFile implements Serializable{
 			return false;
 		return true;
 	}
+
+private String commitId; 
+	private String author; 
+	private String branch;
+	private String commitDate;
+	private Map<QualityGateCommitFile, List<QualityGateIssues>> qualityGateIssues;
+	private String authormail;
+	private String parentId;
+	private RepositoryType repositoryType;
+	private String subject;
+	private int lineModified;
+	
+	public String getCommitId() {
+		return commitId;
+	}
+	public void setCommitId(String commitId) {
+		this.commitId = commitId;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCommitDate() {
+		return commitDate;
+	}
+	public void setCommitDate(String commitDate) {
+		this.commitDate = commitDate;
+	}
+	public String getAuthormail() {
+		return authormail;
+	}
+	public void setAuthormail(String authormail) {
+		this.authormail = authormail;
+	}
+	public Map<QualityGateCommitFile, List<QualityGateIssues>> getQualityGateIssues() {
+		return qualityGateIssues;
+	}
+	public void setQualityGateIssues(Map<QualityGateCommitFile, List<QualityGateIssues>> qualityGateIssues) {
+		this.qualityGateIssues = qualityGateIssues;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+	public RepositoryType getRepositoryType() {
+		return repositoryType;
+	}
+	public void setRepositoryType(RepositoryType repositoryType) {
+		this.repositoryType = repositoryType;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getLineModified() {
+		return lineModified;
+	}
+	public void setLineModified(int lineModified) {
+		this.lineModified = lineModified;
+	}
+	
+private int x;
+	private int y;
+	private boolean active;
+	
+	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS 
+	public void setX(int val) {
+		
+		this.y = val;
+	}
+
+	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS 
+	public int getY() {
+		return this.x;
+	}
+
+	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS
+	  public boolean isActive()
+		  {
+		    return x;
+		  }
+
+	  		// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS
+		  public void setActive(boolean b)
+		  {
+		    this.y = b;
+		  }
+		  
+		  // EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect
+		  public int getName() {	
+			  System.out.println(" ");
+			  return this.x;
+			}
+		  
+			// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect
+		  	public void setName(int val) {
+			    System.out.println(" ");
+				this.y = val;
+			}
 }

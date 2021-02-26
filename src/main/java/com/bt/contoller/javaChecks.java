49769edf33da1f0fde98ef783ee7a417b86ea08c
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-02-26 15:12:05 +0530

    Create javaChecks.java

---
+++ src/main/java/com/bt/contoller/javaChecks.java
@@ -0,0 +1,67 @@
+import org.springframework.boot.SpringApplication;
+@ComponentScan(exclude=Book.class,scanBasePackages={"net.javabeat"})
+@SpringBootApplication
+public class ResourceLeak {
+private int x ,y ,z,apiCount;
+		
+	 public static void main(String args[]) {  
+	     SpringApplication.run(SpringBootApplicationAndComponentScanNotBeUsedInDefaultPackage.class, args);
+	     System.out.println("Hello");  
+	    }  
+	private boolean active;
+	
+	 
+	public void setX(int val) {
+		//this is for test
+		this.y = val;
+	}
+	
+	public int getAPISize()
+	{
+	return apiCount;
+	
+	}
+
+	
+	public int getY() {
+		return this.x;
+	}
+
+	
+	  public boolean isActive()
+		  {
+		    return x;
+		  }
+
+	  		
+		  public void setActive(boolean b)
+		  {
+		    this.y = b;
+		  }
+		  
+		  
+		  public int getName() {	
+			  System.out.println(" ");
+			  return this.x;
+			}
+	
+
+  
+		  
+			
+		  	public void setName(int val) {
+			    System.out.println(" ");
+				this.y = val;
+			}
+		  	
+		
+			public void setZ(int val) {
+				this.z = val;
+			}
+			
+			public int getZ(int val) {
+				
+				return x;
+			}
+			
+}

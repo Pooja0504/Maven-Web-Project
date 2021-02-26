604f2e173fa93cf03b781a707f9ad33e4527c481
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-02-26 15:20:54 +0530

    Update pooja.java

--- src/main/java/com/bt/contoller/pooja.java
+++ src/main/java/com/bt/contoller/pooja.java
@@ -11,29 +11,6 @@ class pooja implements Externalizable {
         age = 10;
     }
  
-    @Override
-    public void writeExternal(ObjectOutput out)
-        throws IOException
-    {
-        out.writeObject(name);
-        out.writeInt(age);
-        out.writeInt(year);
-    }
- 
-    @Override
-    public void readExternal(ObjectInput in)
-        throws IOException, ClassNotFoundException
-    {
-        name = (String)in.readObject();
-        year = in.readInt();
-        age = in.readInt();
-    }
- 
-    @Override public String toString()
-    {
-        return ("Name: " + name + "\n"
-                + "Year: " + year + "\n"
-                + "Age: " + age);
-    }
+  
 }
  

4f16c19f715903720cff7f94a703eccf42c8f029
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-02-26 15:31:21 +0530

    Update pooja.java

--- src/main/java/com/bt/contoller/pooja.java
+++ src/main/java/com/bt/contoller/pooja.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class pooja implements Externalizable { 
-  public Tomato (String color, int weight) {
+  public pooja (String color, int weight) {
   System.out.println("pooja");
   }
 }
